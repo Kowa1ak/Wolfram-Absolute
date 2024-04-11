c892608e4cb3906b49451a3c44ad7b0abb332460
SoTokcuk <89222790+SoTokcuk@users.noreply.github.com>
2024-04-11 13:41:18 +0300

    fixed auth services and reg/login methods to use with spring

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { AuthService } from 'src/app/services/auth.service';
+import { User } from 'src/app/interfaces/auth';
+
 
 @Component({
   selector: 'app-login',
@@ -29,19 +31,18 @@ export class LoginComponent {
   get password() { return this.loginForm.controls['password']; }
   loginUser() {
     const { email, password } = this.loginForm.value;
-    this.authService.getUserByEmail(email as string).subscribe(
+    this.authService.loginUser({ email, password } as User).subscribe(
       response => {
-        if (response.length > 0 && response[0].password === password) {
-          sessionStorage.setItem('email', email as string);
-          this.router.navigate(['/home']);
-        } else {
-          this.msgService.add({ severity: 'error', summary: 'Error', detail: 'email or password is wrong' });
-        }
+        console.log(response);
+        sessionStorage.setItem('email', email as string);
+        sessionStorage.setItem('username', response.username);
+        this.router.navigate(['/home']);
       },
       error => {
-        this.msgService.add({ severity: 'error', summary: 'Error', detail: 'Something went wrong' });
+        console.error(error)
+        this.msgService.add({ severity: 'error', summary: 'Error', detail: 'Invalid email or password' });
       }
-
     )
   }
+  
 }
--- src/app/interfaces/auth.ts
+++ src/app/interfaces/auth.ts
@@ -1,6 +1,7 @@
 export interface User {
     id: string;
-    fullName: string;
+    username: string;
     email: string;
     password: string
+    roles: string
 }
