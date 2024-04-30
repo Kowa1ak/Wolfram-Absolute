a765f866013243ef31159df52e4c5c7488178fe9
Kowa1ak <danila.tsatsuk22@gmail.com>
2024-04-30 14:47:18 +0300

    sending data for the Basic page

--- src/app/components/arithmetic/arithmetic.component.ts
+++ src/app/components/arithmetic/arithmetic.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ElementRef, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl } from '@angular/forms'; // добавьте этот импорт
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-arithmetic',
@@ -8,20 +9,59 @@ import { FormGroup, FormControl } from '@angular/forms'; // добавьте э�
   styleUrls: ['./arithmetic.component.css'],
 })
 export class ArithmeticComponent {
-  form: FormGroup; // добавьте эту строку
+  form: FormGroup;
   @ViewChild('inputArithmetic', { static: false }) inputArithmetic!: ElementRef;
   @ViewChild('btnLibrary', { static: false }) btnLibrary!: ElementRef;
   @ViewChild('btnThread', { static: false }) btnThread!: ElementRef;
-  constructor(private router: Router, private ngZone: NgZone) {
+  constructor(
+    private router: Router,
+    private ngZone: NgZone,
+    private http: HttpClient
+  ) {
     this.form = new FormGroup({
       inputArithmetic: new FormControl(''), // добавьте эту строку
     });
+  }
+  sendData(email: string) {
+    const data = {
+      email: sessionStorage.getItem('email'),
+      expression: this.form.get('inputArithmetic')?.value,
+      library: this.selectedLibrary,
+      threads: this.selectedThread,
+    };
+
+    this.http
+      .post('http://localhost:8080/wolfram/basic', data)
+      .subscribe((response) => {
+        console.log(response);
+      });
   }
 
   navigateToHome() {
     this.router.navigate(['']);
   }
-  funcs = ['sin', 'cos', 'tan', 'ctg'];
+  funcs = [
+    'sin',
+    'cos',
+    'tan',
+    'ctg',
+    'abs',
+    'acos',
+    'asin',
+    'atan',
+    'cbrt',
+    'ceil',
+    'cosh',
+    'exp',
+    'floor',
+    'log',
+    'log10',
+    'log2',
+    'sinh',
+    'sqrt',
+    'tanh',
+    'signum',
+  ];
   showLibraryList = false;
   isFunctionsVisible = false;
   showThreadList = false;
@@ -102,14 +142,23 @@ export class ArithmeticComponent {
       '.additional-buttons'
     ) as HTMLElement;
     if (additionalButtons) {
-      additionalButtons.style.marginRight = '220px';
+      additionalButtons.style.marginRight = '200px';
     }
   }
   insertFunction(func: string) {
-    const currentValue = this.form.get('inputArithmetic')!.value;
-    this.form.get('inputArithmetic')!.setValue(currentValue + func + '()');
+    const inputField = this.form.get('inputArithmetic')!;
+    const currentValue = inputField.value;
+    const cursorPosition = this.inputArithmetic.nativeElement.selectionStart;
+
+    const newValue =
+      currentValue.slice(0, cursorPosition) +
+      func +
+      '()' +
+      currentValue.slice(cursorPosition);
+    inputField.setValue(newValue);
+
     setTimeout(() => {
-      const newPosition = currentValue.length + func.length + 1;
+      const newPosition = cursorPosition + func.length + 1; // 1 for the opening parenthesis
       this.inputArithmetic.nativeElement.focus();
       this.inputArithmetic.nativeElement.setSelectionRange(
         newPosition,
@@ -122,16 +171,60 @@ export class ArithmeticComponent {
     if (event.key === 'Backspace') {
       const currentValue = this.form.get('inputArithmetic')!.value;
       const cursorPosition = this.inputArithmetic.nativeElement.selectionStart;
-      if (
-        currentValue.slice(cursorPosition - 1, cursorPosition) === '(' ||
-        currentValue.slice(cursorPosition - 1, cursorPosition) === ')'
-      ) {
-        const funcEnd = this.findLastFunction(currentValue, cursorPosition);
-        if (funcEnd !== -1) {
+      if (currentValue.slice(cursorPosition - 1, cursorPosition) === ')') {
+        const regex = /(\w+\((?:[^()]*|\((?:[^()]*|\([^()]*\))*\))*\))/g;
+        let match;
+        let lastMatch;
+        while (
+          (match = regex.exec(currentValue.slice(0, cursorPosition))) !== null
+        ) {
+          lastMatch = match;
+        }
+        if (lastMatch) {
           this.form
             .get('inputArithmetic')!
-            .setValue(currentValue.slice(0, funcEnd));
-          event.preventDefault();
+            .setValue(
+              currentValue.slice(0, lastMatch.index) +
+                currentValue.slice(cursorPosition)
+            );
+        }
+      } else {
+        const lastOpenBracket = currentValue
+          .slice(0, cursorPosition)
+          .lastIndexOf('(');
+        const lastCloseBracket = currentValue
+          .slice(0, cursorPosition)
+          .lastIndexOf(')');
+        if (lastOpenBracket !== -1 && lastOpenBracket > lastCloseBracket) {
+          this.form
+            .get('inputArithmetic')!
+            .setValue(
+              currentValue.slice(0, lastOpenBracket) +
+                currentValue.slice(cursorPosition)
+            );
+        } else if (lastOpenBracket !== -1) {
+          const funcStart = currentValue
+            .slice(0, lastOpenBracket)
+            .search(/\W\w*$/);
+          const correspondingClosingBracket = currentValue.indexOf(
+            ')',
+            cursorPosition
+          );
+          if (correspondingClosingBracket !== -1) {
+            this.form
+              .get('inputArithmetic')!
+              .setValue(
+                currentValue.slice(0, funcStart === -1 ? 0 : funcStart + 1) +
+                  currentValue.slice(correspondingClosingBracket + 1)
+              );
+          }
+        } else {
+          this.form
+            .get('inputArithmetic')!
+            .setValue(
+              currentValue.slice(0, cursorPosition - 1) +
+                currentValue.slice(cursorPosition)
+            );
         }
       }
     }
